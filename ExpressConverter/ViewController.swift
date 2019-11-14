38098bd2b65781bff6b7d78964702f9b25ad2b6e
JASI <jasimac@JASIs-Mac.local>
2019-11-14 09:33:00 -0800

    with 1 view

--- ExpressConverter/ViewController.swift
+++ ExpressConverter/ViewController.swift
@@ -8,30 +8,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
- 
-        
-        
-        
+
+
+
+class ViewController: UIViewController {
+   
+    //<-- all the labels --->
+
+    //amount enter by user
+    @IBOutlet weak var useramt: UITextField!
+    
+    //view1
+    @IBOutlet weak var label1: UIView!
+    //country1 flag
+    @IBOutlet weak var countryFlag1: UIImageView!
+    //country1label
+    @IBOutlet weak var countrylabel1: UILabel!
+    
+
+    //view2
+    @IBOutlet weak var label2: UIView!
+    //country2 flag
+    @IBOutlet weak var countryFlag2: UIImageView!
+    //country2 label
+    @IBOutlet weak var countrylabel2: UILabel!
+    
+    //Default and change label
+    @IBOutlet weak var defaultChngeRate: UILabel!
+
+    //resultlabel
+    @IBOutlet weak var showResultlabel: UILabel!
+    
+    
+    //all the buttons
+    
+   //btn to change country 1
+    @IBAction func chngeCountry1btn(_ sender: Any) {
+    }
+    
+    //btn to change country 2
+    @IBAction func chngeCountry2btn(_ sender: Any) {
+    }
+    
+    //button to change rate
+    @IBAction func rateChngebtn(_ sender: Any) {
+    
         
         
     }
-
-
-    
-    
     
     
     @IBAction func Resultbtn(_ sender: Any) {
     
-    
-    print("helloworld")
+        print("convertbtn")
         
-    
     }
+    
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    
+    
+    
+
     
     
     
